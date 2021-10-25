36527973ee2826441dad77a081dfb6e2047cde04
Joshua Elliott <joshua.elliott@enghouse.com>
2021-10-25 22:10:29 +1300

    Initial Check-in

---
+++ MathsProblem/NegPosAddition.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathsProblem
+{
+    public class NegPosAddition
+    {
+        private int m_min;
+        private int m_max;
+        private int m_ansMin;
+        private int m_ansMax;
+        private readonly Random m_random = new Random();
+
+        public string BlankSeparator { get; set; }
+
+        public NegPosAddition(int min, int max, int ansMin, int ansMax)
+        {
+            BlankSeparator = "____";
+
+            m_min = min;
+            m_max = max;
+            if (m_min > m_max)
+                m_min = m_max;
+
+            m_ansMin = ansMin;
+            if (m_ansMin > m_min)
+                m_ansMin = m_min;
+
+            m_ansMax = ansMax;
+            if (m_ansMax < m_max)
+                m_ansMax = m_max;
+        }
+
+        public void GetNextProblem(out int a, out int b, out int answer)
+        {
+            do
+            {
+                GenerateNextProblem(out a, out b, out answer);
+            } while (answer < m_ansMin || answer > m_ansMax);
+        }
+
+        private void GenerateNextProblem(out int a, out int b, out int answer)
+        {
+            a = m_random.Next(m_min, m_max + 1);
+            b = m_random.Next(m_min, m_max + 1);
+            answer = a + b;
+        }
+
+        private int m_solvePos = 0;
+
+        public void GenerateNextProblem(out List<string> questions, out List<string> answers)
+        {
+            questions = new List<string>();
+            answers = new List<string>();
+            GetNextProblem(out var a, out var b, out var answer);
+            questions.Add(m_solvePos == 0 ? BlankSeparator : a.ToString());
+            answers.Add(a.ToString());
+            if (b >= 0)
+            {
+                questions.Add("+");
+                answers.Add("+");
+                questions.Add(m_solvePos == 1 ? BlankSeparator : b.ToString());
+                answers.Add(b.ToString());
+            }
+            else
+            {
+                questions.Add("-");
+                answers.Add("-");
+                b = 0 - b;
+                questions.Add(m_solvePos == 1 ? BlankSeparator : b.ToString());
+                answers.Add(b.ToString());
+            }
+            questions.Add("=");
+            answers.Add("=");
+            questions.Add(m_solvePos == 2 ? BlankSeparator : answer.ToString());
+            answers.Add(answer.ToString());
+
+            ++m_solvePos;
+            if (m_solvePos >= 3)
+                m_solvePos = 0;
+        }
+    }
+}
+
+
